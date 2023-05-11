525b1e0e55ddb2c78b14443043377d4baa78ab93
Deivis <davidesparza9090@gmail.com>
2023-05-10 18:51:36 -0600

    Generacion de SobreNosotros Componente...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgendarComponent } from './agendar/agendar.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AgendarComponent
+    AgendarComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
