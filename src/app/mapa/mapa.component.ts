3741964af5d35e4e0eb3303fc2a5363f1afa4dc1
Daywave <djjceja@gmail.com>
2023-05-10 23:01:01 -0600

    Auto stash before merge of "main" and "origin/main"

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-mapa',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class MapaComponent {
 
+  ngAfterviewInit(): void {
+    const map = new Map('map').setView([51.505, -0.09], 13);
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
 }
